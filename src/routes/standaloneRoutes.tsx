d4676cab7eb8a6569d12b81c959978a21e12a4e8
Tungnqs <thanhlamtainguyen@gmail.com>
2024-09-16 16:34:30 +0700

    delete unused import

--- src/routes/standaloneRoutes.tsx
+++ src/routes/standaloneRoutes.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { IRoute } from "../interfaces";
-import Loader from "../components/Loader/Loader";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
-import { Navigate } from "react-router";
-import PropertyList from "../pages/LandLord/Property/PropertyList";
 import Homepage from "../pages/Homepage/Homepage";
 import PageNotFound from "../pages/PageNotFound/PageNotFound";
 
